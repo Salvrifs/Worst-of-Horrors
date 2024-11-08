19769509ad9f9155ebdcb7885a42c5f3db781b8f
Dmitriy Fedotov <cerberweter@gmail.com>
2024-11-08 15:29:08 +0300

    add NPCBehaviour
    adding NPC behaviour

---
+++ Assets/ScriptsNPC/MainBrain.cs
@@ -0,0 +1,194 @@
+using System.Collections;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.XR;
+
+public class MainBrain : MonoBehaviour
+{
+    [SerializeField] float Chase_Distance = 30f;
+    [SerializeField] float Attack_Distance = 10f;
+
+//Состояния NPC
+    public enum NPCState
+    {
+    Patrolling,
+    Chasing,
+    CollectingItems,
+    Attacking,
+    Idle
+    }
+
+    private NPCState currentState;
+    //Переменные
+    Animator m_animator;
+    NavMeshAgent m_agent;
+    Transform m_player;
+    bool IsChasing, IsAttacking, IsPatrolling;
+    float current_distance;
+
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        InitializeNavMeshAgent();
+        InitializePlayer();
+
+        currentState = GetComponent<PatrollingNPC>() != null ? NPCState.Patrolling : NPCState.CollectingItems;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        current_distance = Vector3.Distance(m_player.position, m_agent.transform.position);
+        
+        switch (currentState)
+    {
+        case NPCState.Patrolling:
+            HandlePatrolling();
+            break;
+        case NPCState.Chasing:
+            HandleChasing();
+            break;
+      /*  case NPCState.CollectingItems:
+            if (TryGetComponent<CollectingNPC>(out var collectBeh))
+            {
+                collectBeh.StartPickUping();
+            } 
+            break; */
+
+    }
+}
+
+private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red; // Цвет сферы
+        Gizmos.DrawWireSphere(transform.position, Chase_Distance); // Рисуем сферу
+    }
+
+
+
+
+    //Инициализация Player
+    private void InitializePlayer()
+    {
+        m_player = GameObject.FindGameObjectWithTag("player")?.transform;
+        if (m_player == null)
+        {
+            Debug.LogError("Error: Player doesn't exist in this World!");
+            return;
+        }
+    }
+
+    //инициализация NavMeshAgent
+    private void InitializeNavMeshAgent()
+    {
+        m_agent = GetComponent<NavMeshAgent>();
+        if (m_agent == null)
+        {
+            Debug.LogError("Error: Player hasn't NavMeshAgent component in this World!");
+            return;
+        }
+    }
+
+    //Корутина для преследования
+    private IEnumerator CoroutinePatrol()
+    {
+        
+        m_agent.ResetPath();
+        yield return new WaitForSeconds(5f);
+        if (TryGetComponent<PatrollingNPC>(out var patrolBehavior))
+        {
+            patrolBehavior.StartPatrolling();
+        }
+        IsPatrolling = true;
+    }
+
+    //Изменение состояния
+    private void ChangeState(NPCState NewState)
+    {
+        currentState = NewState;
+    }
+
+    //Для состояния патрулирования
+    private void HandlePatrolling()
+    {
+        if (TryGetComponent<ChasingNPC>(out var ChaseBeh))
+    {
+        // Преследование на достаточном расстоянии
+        if (current_distance <= Chase_Distance)
+        {
+            ChangeState(NPCState.Chasing);
+        }
+        else 
+        {
+            // Проверка наличия PatrollingNPC
+            if (TryGetComponent<PatrollingNPC>(out var patrolBeh))
+            {
+                patrolBeh.StartPatrolling();
+            }
+            else
+            {
+                Debug.LogError("PatrollingNPC component is missing!");
+            }
+        }
+    }
+    
+        else
+    {
+        // Проверка наличия PatrollingNPC
+        if (TryGetComponent<PatrollingNPC>(out var patrolBeh))
+        {
+            patrolBeh.StartPatrolling();
+        }
+        else
+        {
+            Debug.LogError("PatrollingNPC component is missing!");
+        }
+    }
+    }
+
+    //Для состояния преследования
+    private void HandleChasing()
+    {
+        if (TryGetComponent<ChasingNPC>(out var ChaseBeh))
+        {
+            //ещё не преследуется
+            if (!IsChasing)
+            {
+                if (current_distance <= Chase_Distance)
+                {
+                    IsChasing = true;
+                    ChangeState(NPCState.Chasing);
+                    ChaseBeh.StartChasing();
+                }
+            }
+
+            //уже преследуется
+            else
+            {
+                //Преследование
+                if (current_distance <= Chase_Distance)
+                {
+                    ChaseBeh.StartChasing();
+                }
+                //атака
+              /*  else if (current_distance <= Attack_Distance && TryGetComponent<AttackNPC>(out var AttackBeh))
+                {
+                    AttackBeh.TakeAttack();
+                }*/
+                //отмена преследования
+                else 
+                {
+                    IsChasing = false;
+                    ChangeState(NPCState.Patrolling);
+                }
+            }
+
+            
+        }
+    }
+
+
+
+
+}
